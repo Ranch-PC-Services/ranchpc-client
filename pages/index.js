187fc3446c820d8000c76f231fd63a5cf2d49580
Isaac Sarte <isaacs@mosaic-solutions.com>
2022-07-16 22:46:25 +0800

    fixed header layout

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,37 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import MouseTrail from '@pjsalita/react-mouse-trail';
+import mouseTrailConfig from '../config/mousetrail/mousetrail.json';
 
 // Components
 import Navbar from '../components/Navbar';
+import Main from '../components/Main';
 import Footer from '../components/Footer';
 
 const Home = () => {
   return (
     <div>
 
+      <MouseTrail config={mouseTrailConfig} />
+
       <Head>
         <title>Ranch PC Services</title>
         <meta name="description" content="Build Your Personal Computer that Satisfies You with Us" />
       </Head>
 
+      {/* mouse trail */}
+
       {/* Navbar */}
-      <Navbar />
+      <div>
+        <Navbar />
+      </div>
 
       {/* Main */}
+      <Main />
 
       {/* Footer */}
-      <Footer />
+      <div className="overflow-hidden z-[100000]">
+        <Footer />
+      </div>
 
     </div>
   )
