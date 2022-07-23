46aa0bcad51b15945d541164e1c76dd4b092df4e
Isaac Sarte <isaacs@mosaic-solutions.com>
2022-07-23 16:36:56 +0800

    sass config

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,15 +1,17 @@
+import styles from '../styles/landing/Footer.module.scss';
+
 // Components
 import ContactIcons from './common/ContactIcons';
 
 const Footer = () => {
   return (
     <footer className="bg-[#222] text-center">
-      <div className="footer-container text-white font-bold">
+      <div className={styles.footer__container}>
         <p className="text-[2rem] mt-5">Ranch PC Services</p>
 
         <ContactIcons />
 
-        <div className="footer-container__sub bg-[#0a0a0a]">
+        <div className={styles.footer__containerSub}>
           <span className="text-[1.2rem]">Copyright 2020 {""}
             <a href="#home" className="text-[#637CBB]">Ranch PC</a> {""}
             all rights reserved.
--- next.config.js
+++ next.config.js
@@ -4,9 +4,14 @@ const nextConfig = {
   swcMinify: true,
 }
 
+const path = require('path')
+
 module.exports = {
   images: {
     loader: 'akamai',
     path: '',
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
   },
 }
